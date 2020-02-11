ad376257ea60d7eab6c0ff1d523e6943b61ee6e2
prevelat <41985981+prevelat@users.noreply.github.com>
2020-02-10 16:14:21 -0800

    Add files via upload

---
+++ d00/ex01/Cube.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    [SerializeField] char c;
+    public GameObject End;
+    private float speed;
+    public Vector3 InitialPosition;
+    private bool newspeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitialPosition = transform.position;
+        newspeed = true;
+    }
+
+    public void Reset()
+    {
+        transform.position = InitialPosition;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (newspeed)
+        {
+            speed = Random.Range(5f, 10f);
+            newspeed = false;
+        }
+        if (gameObject.activeSelf)
+        {
+            if (transform.position.y < 2)
+            {
+                if (c == 'A' && Input.GetKeyDown(KeyCode.A))
+                {
+                    newspeed = true;
+                    print("Precision : " + (gameObject.transform.position.y - End.transform.position.y));
+                    Reset();
+                }
+                if (c == 'S' && Input.GetKeyDown(KeyCode.S))
+                {
+                    newspeed = true;
+                    print("Precision : " + (gameObject.transform.position.y - End.transform.position.y));
+                    Reset();
+                }
+                if (c == 'D' && Input.GetKeyDown(KeyCode.D))
+                {
+                    newspeed = true;
+                    print("Precision : " + (gameObject.transform.position.y - End.transform.position.y));
+                    Reset();
+                }
+            }
+            transform.position += new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ d00/ex01/CubeSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cube;
+    private float countdown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = Random.Range(1f, 2f);
+    }
+    GameObject Create(GameObject spawn)
+    {
+        return Instantiate(spawn);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0 && !cube.activeSelf)
+        {
+            countdown = 3f;
+            Create(cube);
+            cube.SetActive(true);
+        }
+    }
+}
